d420907005ff1747185a07d85f4dcb385a43a63f
Piyush Kumar <piyushat115@gmail.com>
2024-12-23 18:45:53 +0530

    fixed linking in pages

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,33 +8,33 @@ export default function Footer() {
         <div className="container">
           <p>&copy; 2024 Piyush Kumar. All Rights Reserved.</p>
           <div className="footer-links space-x-4 mb-3">
-            <Link href="/" passHref>
-              <a className="text-white hover:underline">Home</a>
+            <Link href="/" passHref className="text-white hover:underline">
+              Home
             </Link>
-            <Link href="/about" passHref>
-              <a className="text-white hover:underline">About</a>
+            <Link href="/about" passHref className="text-white hover:underline">
+              About
             </Link>
-            <Link href="/blogs" passHref>
-              <a className="text-white hover:underline">Blog</a>
+            <Link href="/blogs" passHref className="text-white hover:underline">
+              Blog
             </Link>
-            <Link href="/contact" passHref>
-              <a className="text-white hover:underline">Contact</a>
+            <Link href="/contact" passHref className="text-white hover:underline">
+              Contact
             </Link>
           </div>
 
           <div className="social-links space-x-4">
-            <a href="https://www.facebook.com/ipiyushkumar001/" target="_blank" className="text-white">
+            <Link href="https://www.facebook.com/ipiyushkumar001/" target="_blank" className="text-white">
               <i className="fa fa-facebook"></i>
-            </a>
-            <a href="https://x.com/SamaMaou67248" target="_blank" className="text-white">
+            </Link>
+            <Link href="https://x.com/SamaMaou67248" target="_blank" className="text-white">
               <i className="fa fa-twitter"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/piyush-kumar-4199a5202/" target="_blank" className="text-white">
+            </Link>
+            <Link href="https://www.linkedin.com/in/piyush-kumar-4199a5202/" target="_blank" className="text-white">
               <i className="fa fa-linkedin"></i>
-            </a>
-            <a href="https://www.instagram.com/ipiyushkumar001/" target="_blank" className="text-white">
+            </Link>
+            <Link href="https://www.instagram.com/ipiyushkumar001/" target="_blank" className="text-white">
               <i className="fa fa-instagram"></i>
-            </a>
+            </Link>
           </div>
         </div>
       </footer>
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,15 +1,22 @@
 import Link from 'next/link';
 
 export default function Navigation() {
+  const links = [
+    {link:"/", name: "Home"},
+    {link:"/about", name: "About Me"},
+    {link:"/services", name: "Services"},
+    {link:"/blogs", name: "Blogs"},
+    {link:"/gallery", name: "Gallery"},
+    {link:"/socials", name: "Socials"},
+    {link:"/contact", name: "Contact"},
+  ]
   return (
     <div>
       {/* Navigation Bar */}
       <nav className="bg-white dark:bg-black shadow-md">
         <div className="max-w-screen-xl mx-auto px-4 py-3 flex justify-between items-center">
-          <Link href="/" passHref>
-            <a className="text-2xl font-semibold text-primary dark:text-white">
+          <Link href="/" passHref className="text-2xl font-semibold text-primary dark:text-white">
               Piyush Kumar
-            </a>
           </Link>
           <button className="sm:hidden px-3 py-2 text-white" id="navbar-toggle">
             <span className="block w-6 h-0.5 bg-gray-700 dark:bg-white mb-1"></span>
@@ -17,41 +24,11 @@ export default function Navigation() {
             <span className="block w-6 h-0.5 bg-gray-700 dark:bg-white"></span>
           </button>
           <div className="hidden sm:flex space-x-6">
-            <Link href="/" passHref>
-              <a className="text-lg text-primary dark:text-white hover:underline">
-                Home
-              </a>
-            </Link>
-            <Link href="/about" passHref>
-              <a className="text-lg text-primary dark:text-white hover:underline">
-                About Me
-              </a>
-            </Link>
-            <Link href="/services" passHref>
-              <a className="text-lg text-primary dark:text-white hover:underline">
-                Services
-              </a>
-            </Link>
-            <Link href="/blogs" passHref>
-              <a className="text-lg text-primary dark:text-white hover:underline">
-                Blog
-              </a>
-            </Link>
-            <Link href="/gallery" passHref>
-              <a className="text-lg text-primary dark:text-white hover:underline">
-                Gallery
-              </a>
-            </Link>
-            <Link href="/socials" passHref>
-              <a className="text-lg text-primary dark:text-white hover:underline">
-                Socials
-              </a>
-            </Link>
-            <Link href="/contact" passHref>
-              <a className="text-lg text-primary dark:text-white hover:underline">
-                Contact
-              </a>
-            </Link>
+            {links.map((link, index) => (
+              <Link href={link.link} key={index} passHref className="text-lg text-primary dark:text-white hover:underline">
+                {link.name}
+              </Link>
+            ))}
           </div>
         </div>
       </nav>
