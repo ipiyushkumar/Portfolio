2ec4df2f32f80c2dfe5e1e4d53fdf0bf389c9d8b
Piyush Kumar <piyushat115@gmail.com>
2024-12-23 19:36:13 +0530

    wrote my first blog

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,9 +1,13 @@
 // pages/blogs.tsx
 
+import Link from "next/link";
 import Navigation from "@/components/navigation";
 import Footer from "@/components/footer";
 
 export default function Blogs() {
+  const blogCards = [
+    {title: "Understanding Web Development Trends in 2025", link: "/blogs/understanding-web-development-trends-in-2024", date: "January 01, 2024", description: "I made this portfolio website to showcase my skills and projects..."},
+  ]
   return (
     <div className="min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Navigation />
@@ -14,18 +18,20 @@ export default function Blogs() {
         {/* Blog Post Cards */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           <div className="bg-white shadow-md rounded-lg p-6">
-            <div>
-              <h3 className="text-xl font-semibold text-black mb-4">
-                <a href="#">
-                  Understanding Web Development Trends in 2024
-                </a>
-              </h3>
-              <p className="text-muted text-sm mb-4 text-black">August 27, 2024</p>
-              <p className="text-justify mb-4 text-black">I made this portfolio website to showcase my skills and projects...</p>
-              <a href="#" className="btn btn-primary inline-block bg-blue-600 py-2 px-4 rounded-full">
-                Read More
-              </a>
-            </div>
+            {blogCards.map((blog, index) => (
+              <div key={index}>
+                <h3 className="text-xl font-semibold text-black mb-4">
+                  <Link href={blog.link} passHref>
+                    {blog.title}
+                  </Link>
+                </h3>
+                <p className="text-muted text-sm mb-4 text-black">{blog.date}</p>
+                <p className="text-justify mb-4 text-black">{blog.description}</p>
+                <Link href={blog.link} passHref className="btn btn-primary inline-block bg-blue-600 py-2 px-4 rounded-full">
+                  Read More
+                </Link>
+              </div>
+            ))}
           </div>
           {/* Add more blog posts here using the same structure */}
         </div>
@@ -34,3 +40,23 @@ export default function Blogs() {
     </div>
   );
 }
+
+
+/**
+ * blog starter template
+ */
+
+
+// import Navigation from "@/components/navigation"
+// import Footer from "@/components/footer"
+// export default function blog () {
+//   return (
+//     <div className="min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+//       <Navigation />
+//         <section className="max-w-7xl mx-auto px-4">
+//           this is my first blog
+//         </section>
+//       <Footer />
+//     </div>
+//   )
+// }
