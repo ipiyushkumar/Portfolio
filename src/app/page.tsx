529548b2fea8fc8b4d99d47884a0b132b673da7d
Piyush Kumar <piyushat115@gmail.com>
2025-01-19 19:11:11 +0530

    updated headline of the home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,8 +35,8 @@ export default function Home() {
     <div className="min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
       {/* Main Section */}
       <div className="flex flex-col text-center mt-5">
-        <h1 className="text-4xl font-bold text-primary dark:text-white">Piyush Kumar</h1>
-        <h4 className="text-5xl text-danger">FULL Stack Developer</h4>
+        <h1 className="text-4xl font-extrabold text-primary dark:text-white">Piyush Kumar</h1>
+        <h4 className="text-5xl text-danger">Full stack web developer</h4>
         <p className="text-lg mt-4">
           Hi, I&#39;m Piyush Kumar, a passionate Developer ready to bring your ideas to life.
           I am an Engineer that specializes in Software and web development. I make high quality web sites in all manner of technologies such as NEXT JS, React JS, ExpressJS, Spring Boot. i also code in several languages such as JavaScript, TypeScript, Java, Python, C, C++, C# and other languages.
