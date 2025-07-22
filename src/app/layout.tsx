5108836a3b7bd28e7148e5a60117ae80ad5cb646
ipiyushkumar <piyushat115@gmail.com>
2025-07-22 20:56:46 +0530

    website restored removed contact and services for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Navigation from "@/components/navigation";
+import Footer from "@/components/footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -15,17 +17,24 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Piyush Kumar",
-  description: "I am an Engineer that specializes in Software and web development. I make high quality web sites in all manner of technologies such as NEXT JS, React JS, ExpressJS, Spring Boot. i also code in several languages such as JavaScript, TypeScript, Java, Python, C, C++, C# and other languages.",
+  description:
+    "I am an Engineer that specializes in Software and web development. I make high quality web sites in all manner of technologies such as NEXT JS, React JS, ExpressJS, Spring Boot. i also code in several languages such as JavaScript, TypeScript, Java, Python, C, C++, C# and other languages.",
 };
 
-export default function RootLayout() {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
         <SpeedInsights />
-        Site Shutdown
+        <Navigation />
+        <div className="sm:mx-72">{children}</div>
+        <Footer />
       </body>
     </html>
   );
